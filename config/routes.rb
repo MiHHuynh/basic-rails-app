a3a056bb5665ea5c42cef4ed76b28f2b27115ec7
Michelle Huynh <michehuynh@yahoo.com>
2016-06-10 15:29:28 -0700

    Edited new product and edit product forms; added search bar into nav; styled with bootstrap. Navbar search needs work

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'products#index'
+  root 'static_pages#index'
 
   # Route for orders
   resources :orders, only: [:index, :show, :create, :destroy]
