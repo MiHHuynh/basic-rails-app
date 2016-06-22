68ed1a9606da3d78f42f7e6bf1ca2c8990917822
Michelle Huynh <michehuynh@yahoo.com>
2016-06-21 23:16:19 -0700

    added mailer and thank you view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,17 @@ class StaticPagesController < ApplicationController
 	def index
 	  @products = Product.limit(3)
 	end
+
+	ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'to@domain.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body.').deliver_now
+
+	def thank_you
+	  @name = params[:name]
+	  @email = params[:email]
+	  @message = params[:message]
+	  ActionMailer::Base.mail(:from => @email,
+      :to => 'your@email.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+	end
 end
 
