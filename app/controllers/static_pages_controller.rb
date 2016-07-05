7c6546d371824493a30f8d26afba08281feb825d
Michelle Huynh <michehuynh@yahoo.com>
2016-07-05 01:23:16 -0700

    rechecked setup for mailers

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,14 +3,14 @@ class StaticPagesController < ApplicationController
 	  @products = Product.limit(3)
 	end
 
-	ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'to@domain.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body.').deliver_now
+	# ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'to@domain.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body.').deliver_now
 
 	def thank_you
 	  @name = params[:name]
 	  @email = params[:email]
 	  @message = params[:message]
 	  ActionMailer::Base.mail(:from => @email,
-      :to => 'your@email.com',
+      :to => 'michehuynh@yahoo.com',
       :subject => "A new contact form message from #{@name}",
       :body => @message).deliver_now
 	end
