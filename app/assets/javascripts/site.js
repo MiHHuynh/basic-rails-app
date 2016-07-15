c9d721cb40f3b1a935c5c47d22bc834b85fce5d5
Michelle Huynh <michehuynh@yahoo.com>
2016-07-14 20:22:33 -0700

    finally fixed the stupid comment form. had double fields for rating

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,7 +1,8 @@
 $(document).on('ready page:load', function(){
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets', readOnly: true,
-        score: function() {
+    $('.rated').raty({ path: '/assets',
+      readOnly: true,
+      score: function() {
         return $(this).attr('data-score');
       }
     });
