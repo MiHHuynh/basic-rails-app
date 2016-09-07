f13ae46107ff63b43b515ebf7dd2b215e1b79890
Michelle Huynh <michehuynh@yahoo.com>
2016-09-07 01:38:51 -0700

    added delete to comments, edited ability.rb to only allow admins to delete

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -31,5 +31,10 @@ class Ability
 
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
+    if user.admin?
+      can :delete, comment
+    else
+      can :read, :all
+    end
   end
 end
