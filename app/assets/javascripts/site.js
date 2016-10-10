7161bc0e143fb02a795056028bb8ec6b08da1dd3
Michelle Huynh <michehuynh@yahoo.com>
2016-10-09 23:47:20 -0700

    put single comment in separate partial; use ajax to load comments

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,11 +1,14 @@
-$(document).on('ready page:load', function(){
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
-
+$(document).on('ready page:load ajaxSuccess', function(){
+    refreshRating();
     $('.image-zoom').elevateZoom();
 });
+
+var refreshRating = function() {
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets',
+        readOnly: true,
+        score: function() {
+            return $(this).attr('data-score');
+        }
+    });
+};
