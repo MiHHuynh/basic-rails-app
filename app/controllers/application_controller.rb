3ca0b4b1a168259000cba5d1e87112e2d789560d
Michelle Huynh <michehuynh@yahoo.com>
2016-10-12 17:50:33 -0700

    upgrade instructions 2.2, 2.3, 2.4, 2.12 for rails guide

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 
   protect_from_forgery with: :exception
+  protect_from_forgery prepend: true # ? what is this for?
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, :alert => exception.message
